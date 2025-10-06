4f01f21fae8c6fd81578d130e8447350319b72bf
Andrew213 <a.kochanov31@yandex.ru>
2025-10-06 12:38:22 +0300

    удалил ненужное

--- apps/api/src/posts-editing/dto/update-posts-editing.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePostsEditingDto } from './create-posts-editing.dto';
-
-export class UpdatePostsEditingDto extends PartialType(CreatePostsEditingDto) {}
